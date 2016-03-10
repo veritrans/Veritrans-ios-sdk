b1f916d8a4b7fcd5e350691aa970719584856b75
Nanang R <nanang@onebitmedia.com>
2016-03-10 17:59:54 +0700

    Implement VTClient for generating token and registering card

--- MidtransCoreKit/MidtransCoreKit/VTClient.m
+++ MidtransCoreKit/MidtransCoreKit/VTClient.m
@@ -7,6 +7,16 @@
 //
 
 #import "VTClient.h"
+#import "VTConfig.h"
+#import "VTNetworking.h"
+#import "VT3DSController.h"
+#import "VTHelper.h"
+
+@interface VTClient() <VT3DSControllerDelegate>
+@property (nonatomic, readwrite) NSString *tokenId;
+@property (nonatomic, strong) VT3DSController *secureController;
+@property (nonatomic, copy) void (^generateTokenCompletion)(id response, NSError *error);
+@end
 
 @implementation VTClient
 
@@ -22,12 +32,62 @@
     return instance;
 }
 
-- (void)generateTokenForCreditCard:(VTCreditCard *)creditCard completion:(void (^)(NSString *, NSError *))callback {
+- (void)generateToken:(VTTokenRequest *)tokenRequest
+           completion:(void (^)(NSString *token, NSError *error))completion {
+    NSString *URL = [NSString stringWithFormat:@"%@/%@", [CONFIG baseUrl], @"token"];
     
+    [[VTNetworking sharedInstance] getFromURL:URL parameters:[tokenRequest dictionaryValue] callback:^(id response, NSError *error) {
+        if (error) {
+            if (completion) completion(nil, error);
+        } else {
+            __weak VTClient *wself = self;
+            
+            //set token id
+            wself.tokenId = response[@"token_id"];
+            
+            //set callback
+            self.generateTokenCompletion = completion;
+            
+            //present 3ds dialogue
+            if (!self.secureController) {
+                self.secureController = [[VT3DSController alloc] init];
+            }
+            self.secureController.webDelegate = self;
+            self.secureController.webUrl = [NSURL URLWithString:response[@"redirect_url"]];
+            UINavigationController *navigation = [[UINavigationController alloc] initWithRootViewController:self.secureController];
+            [[UIApplication rootViewController] presentViewController:navigation animated:YES completion:nil];
+        }
+    }];
 }
 
-- (void)registerCreditCard:(VTCreditCard *)creditCard completion:(void (^)(NSString *, NSError *))callback {
+- (void)registerCreditCard:(VTCreditCard *)creditCard
+                completion:(void (^)(VTRegisteredCreditCard *registeredCard, NSError *))completion {
+    NSString *URL = [NSString stringWithFormat:@"%@/%@", [CONFIG baseUrl], @"card/register"];
+    NSDictionary *param = @{@"card_number":creditCard.number,
+                            @"card_exp_month":creditCard.expiryMonth,
+                            @"card_exp_year":creditCard.expiryYear};
     
+    [[VTNetworking sharedInstance] getFromURL:URL parameters:param callback:^(id response, NSError *error) {
+        [VTHelper handleResponse:response completion:^(id response, NSError *error) {
+            if (response) {
+                VTRegisteredCreditCard *registeredCard = [VTRegisteredCreditCard registeredCardFromData:response];
+                if (completion) completion(registeredCard, nil);
+            } else {
+                if (completion) completion(nil, error);
+            }
+        }];
+    }];
 }
+
+#pragma mark - VT3DSControllerDelegate
+
+- (void)viewController_didFinishTransaction:(VT3DSController *)viewController {
+    [viewController dismissViewControllerAnimated:YES completion:nil];
+    
+    if (self.generateTokenCompletion) {
+        self.generateTokenCompletion(self.tokenId, nil);
+    }
+}
+
 
 @end

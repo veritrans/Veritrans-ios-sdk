e55e5e7ff84edaa5850303ebecab9df07494c455
Nanang R <nanang@onebitmedia.com>
2016-03-15 14:40:00 +0700

    Implement view for alert background

---
+++ MidtransKit/MidtransKit/VTAlertBackgroundView.h
@@ -0,0 +1,13 @@
+//
+//  VTAlertBackgroundView.h
+//  MidtransKit
+//
+//  Created by Nanang Rafsanjani on 3/14/16.
+//  Copyright © 2016 Veritrans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VTAlertBackgroundView : UIView
+
+@end

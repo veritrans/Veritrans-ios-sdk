adebf5dd6ad101d06b50b615c0a17ef0a36493d0
Nanang R <nanang@onebitmedia.com>
2016-03-15 18:25:44 +0700

    Set default value for card payment options to One click

---
+++ Demo/VTDirectDemo/OptionViewController.m
@@ -0,0 +1,37 @@
+//
+//  OptionViewController.m
+//  VTDirectDemo
+//
+//  Created by Nanang Rafsanjani on 3/15/16.
+//  Copyright © 2016 Veritrans. All rights reserved.
+//
+
+#import "OptionViewController.h"
+
+@interface OptionViewController ()
+
+@end
+
+@implementation OptionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

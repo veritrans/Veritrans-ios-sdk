ac6ed7e4ac4db5cc10d568687444d14c5d14d82c
Nanang R <nanang@onebitmedia.com>
2016-03-16 10:55:13 +0700

    Implement VTPaymentResult to UI Flow & add new properties

--- MidtransCoreKit/MidtransCoreKit/VTPaymentResult.m
+++ MidtransCoreKit/MidtransCoreKit/VTPaymentResult.m
@@ -7,6 +7,7 @@
 //
 
 #import "VTPaymentResult.h"
+#import "VTHelper.h"
 
 @interface VTPaymentResult()
 
@@ -17,10 +18,40 @@
 @property(nonatomic, readwrite) NSString *orderId;
 @property(nonatomic, readwrite) NSString *paymentType;
 @property(nonatomic, readwrite) NSDictionary *additionalData;
+@property(nonatomic, readwrite) NSDate *transactionTime;
+@property(nonatomic, readwrite) NSNumber *grossAmount;
 
 @end
 
 @implementation VTPaymentResult
+
+- (instancetype)initWithPaymentResponse:(NSDictionary *)response {
+    self = [super init];
+    if (self) {
+        NSMutableDictionary *mResponse = [NSMutableDictionary dictionaryWithDictionary:response];
+        self.statusCode = [[mResponse objectThenDeleteForKey:@"status_code"] integerValue];
+        self.statusMessage = [mResponse objectThenDeleteForKey:@"status_message"];
+        self.transactionId = [mResponse objectThenDeleteForKey:@"transaction_id"];
+        self.transactionStatus = [mResponse objectThenDeleteForKey:@"transaction_status"];
+        self.orderId = [mResponse objectThenDeleteForKey:@"order_id"];
+        self.paymentType = [mResponse objectThenDeleteForKey:@"payment_type"];
+        
+        id rawGrossAmount = [mResponse objectThenDeleteForKey:@"gross_amount"];
+        if (rawGrossAmount) {
+            self.grossAmount = @([rawGrossAmount doubleValue]);
+        }
+        
+        id rawTransactionTime = [mResponse objectThenDeleteForKey:@"transaction_time"];
+        if (rawTransactionTime) {
+            NSDateFormatter *formatter = [NSObject dateFormatterWithIdentifier:@"vt.date"];
+            formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+            self.transactionTime = [formatter dateFromString:rawTransactionTime];
+        }
+        
+        self.additionalData = mResponse;
+    }
+    return self;
+}
 
 - (instancetype)initWithStatusCode:(NSInteger)statusCode statusMessage:(NSString *)statusMessage transactionId:(NSString *)transactionId transactionStatus:(NSString *)transactionStatus orderId:(NSString *)orderId paymentType:(NSString *)paymentType additionalData:(NSDictionary *)additionalData {
     if (self = [super init]) {
--- MidtransKit/MidtransKit/VTPaymentStatusViewModel.m
+++ MidtransKit/MidtransKit/VTPaymentStatusViewModel.m
@@ -15,52 +15,31 @@
 @property (nonatomic) NSString *orderId;
 @property (nonatomic) NSString *transactionTime;
 @property (nonatomic) NSString *paymentType;
-@property (nonatomic) NSDictionary *paymentStatusData;
+@property (nonatomic) VTPaymentResult *paymentResult;
 
 @end
 
 @implementation VTPaymentStatusViewModel
 
-/*
- "transaction_id" => "ca5e0ff5-4297-48ef-aa25-b3ab018d4ead",
- "order_id" => "order52",
- "gross_amount" => "200000.00",
- "payment_type" => "credit_card",
- "transaction_time" => "2014-03-27 17:20:53",
- "transaction_status" => "capture",
- "fraud_status" => "accept",
- "masked_card" => "481111-1114",
- "saved_token_id" => "4011117d16c884-2cc7-4624-b0a8-10273b7f6cc8",
- "saved_token_id_expired_at" => "2024-03-27 17:20:53",
- "status_code" => "200",
- "bank" => "bni",
- "eci" => "05",
- "approval_code" => "140540",
- "status_message" => "Success, Credit Card 3D Secure transaction is successful"
- */
-
-+ (instancetype)viewModelWithData:(NSDictionary *)paymentStatusData {
++ (instancetype)viewModelWithPaymentResult:(VTPaymentResult *)paymentResult {
     VTPaymentStatusViewModel *viewModel = [[VTPaymentStatusViewModel alloc] init];
-    viewModel.paymentStatusData = paymentStatusData;
+    viewModel.paymentResult = paymentResult;
     return viewModel;
 }
 
-- (void)setPaymentStatusData:(NSDictionary *)paymentStatusData {
-    _paymentStatusData = paymentStatusData;
+- (void)setPaymentResult:(VTPaymentResult *)paymentResult {
+    _paymentResult = paymentResult;
     
     NSNumberFormatter *nformatter = [NSObject numberFormatterWith:@"vt.number"];
-    double damount = [paymentStatusData[@"gross_amount"] doubleValue];
-    self.totalAmount = [NSString stringWithFormat:@"Rp %@", [nformatter stringFromNumber:@(damount)]];
+    self.totalAmount = [NSString stringWithFormat:@"Rp %@", [nformatter stringFromNumber:paymentResult.grossAmount]];
     
-    self.orderId = paymentStatusData[@"order_id"];
+    self.orderId = paymentResult.orderId;
     
-    NSDateFormatter *formatter = [NSDateFormatter new];
-    formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss";
-    NSDate *date = [formatter dateFromString:paymentStatusData[@"transaction_time"]];
+    NSDateFormatter *formatter = [NSObject dateFormatterWithIdentifier:@"vt.date"];
     formatter.dateFormat = @"dd/MM/yyyy, HH:mm:ss";
-    self.transactionTime = [formatter stringFromDate:date];
+    self.transactionTime = [formatter stringFromDate:paymentResult.transactionTime];
     
-    NSString *paymentTypeString = [paymentStatusData[@"payment_type"] stringByReplacingOccurrencesOfString:@"_" withString:@" "];
+    NSString *paymentTypeString = [paymentResult.paymentType stringByReplacingOccurrencesOfString:@"_" withString:@" "];
     self.paymentType = [paymentTypeString capitalizedString];
 }
 

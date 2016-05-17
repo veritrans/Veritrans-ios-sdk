613b106ea058f422e69999de12dd144ec2a03817
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2016-05-17 13:38:59 +0700

    Add keyboard accessory for navigation between text fields

---
+++ MidtransKit/MidtransKit/VTKeyboardAccessoryView.h
@@ -0,0 +1,13 @@
+//
+//  VTKeyboardAccessoryView.h
+//  iossdk-gojek
+//
+//  Created by Nanang Rafsanjani on 4/8/16.
+//  Copyright © 2016 Veritrans. All rights reserved.
+//
+
+#import "VTXibView.h"
+
+@interface VTKeyboardAccessoryView : VTXibView
+- (id)initWithFrame:(CGRect)frame fields:(NSArray *)fields;
+@end

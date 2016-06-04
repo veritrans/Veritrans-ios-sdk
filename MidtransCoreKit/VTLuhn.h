c0fa25348ccb2298095d7c921e5b150a228340ae
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2016-06-04 22:47:51 +0700

    Create luhn creditcard validator

---
+++ MidtransCoreKit/VTLuhn.h
@@ -0,0 +1,16 @@
+//
+//  VTLuhn.h
+//  MidtransCoreKit
+//
+//  Created by Nanang Rafsanjani on 6/4/16.
+//  Copyright © 2016 Veritrans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface VTLuhn : NSObject
+
++ (BOOL) validateString:(NSString *) string;
+
+@end

fecae159f928d9b19fc2b6e232ee4badbf16e6ea
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2016-06-10 12:32:29 +0700

    Pass VTMaskedCreditCard to VTTwoClickController
    needed to validate cvv based on credit card number

--- MidtransKit/MidtransKit/VTTwoClickController.h
+++ MidtransKit/MidtransKit/VTTwoClickController.h
@@ -7,12 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MidtransCoreKit/MidtransCoreKit.h>
+
 #import "VTPaymentController.h"
 #import "VTCCBackView.h"
 
 @interface VTTwoClickController : VTPaymentController
 @property (weak, nonatomic) IBOutlet VTCCBackView *backView;
 
-- (instancetype)initWithCustomerDetails:(VTCustomerDetails *)customerDetails itemDetails:(NSArray<VTItemDetail *> *)itemDetails transactionDetails:(VTTransactionDetails *)transactionDetails savedToken:(NSString *)savedToken;
+- (instancetype)initWithCustomerDetails:(VTCustomerDetails *)customerDetails itemDetails:(NSArray<VTItemDetail *> *)itemDetails transactionDetails:(VTTransactionDetails *)transactionDetails maskedCard:(VTMaskedCreditCard *)maskedCard;
 
 @end

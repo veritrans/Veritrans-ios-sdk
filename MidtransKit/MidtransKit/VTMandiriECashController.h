baef80d5d341d7faa1827716689a95a0022a05ff
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2016-06-13 17:07:07 +0700

    Create UI for mandiri ecash payment

---
+++ MidtransKit/MidtransKit/VTMandiriECashController.h
@@ -0,0 +1,13 @@
+//
+//  VTMandiriECashController.h
+//  MidtransKit
+//
+//  Created by Nanang Rafsanjani on 6/13/16.
+//  Copyright © 2016 Veritrans. All rights reserved.
+//
+
+#import "VTPaymentController.h"
+
+@interface VTMandiriECashController : VTPaymentController
+
+@end

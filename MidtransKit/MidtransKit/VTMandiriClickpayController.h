cd41fa73767213ce0e71735683525c9b45cbaeb1
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2016-06-14 15:33:46 +0700

    Refactor & refine clickpay payment controller

--- MidtransKit/MidtransKit/VTMandiriClickpayController.h
+++ MidtransKit/MidtransKit/VTMandiriClickpayController.h
@@ -9,6 +9,6 @@
 #import <UIKit/UIKit.h>
 #import "VTPaymentController.h"
 
-@interface VTClickpayController : VTPaymentController
+@interface VTMandiriClickpayController : VTPaymentController
 
 @end

f29db4df706df6e418b01849806afacf321566c8
Arie <arie@Aries-MacBook-Pro.local>
2016-06-14 13:49:27 +0700

    update fix vt constant

--- MidtransCoreKit/MidtransCoreKit/VTConstant.h
+++ MidtransCoreKit/MidtransCoreKit/VTConstant.h
@@ -41,9 +41,9 @@ static NSString * const VT_MESSAGE_CLIENT_KEY_NOT_SET = @"Please set your Veritr
 
 
 static NSString *const VT_SANDBOX_API_URL = @"https://api.sandbox.veritrans.co.id/v2";
-static NSString *const VT_SANDBOX_MIXPANEL = @"0269722c477a0e085fde32e0248c6003";
+static NSString *const VT_SANDBOX_MIXPANEL = @"cc005b296ca4ce612fe3939177c668bb";
 static NSString *const VT_PRODUCTION_API_URL = @"https://api.veritrans.co.id/v2";
-static NSString *const VT_PRODUCTION_MIXPANEL = @"cc005b296ca4ce612fe3939177c668bb";
+static NSString *const VT_PRODUCTION_MIXPANEL = @"0269722c477a0e085fde32e0248c6003";
 
 static NSString *const VT_TRACKING_CC_TOKEN = @"token";
 static NSString *const VT_TRACKING_PAYMENT_METHOD = @"payment_method";

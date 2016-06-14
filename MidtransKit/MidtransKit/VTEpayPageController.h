af71d93ce43cc4fce9b26382ebca44004036b79d
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2016-06-14 11:14:47 +0700

    Create webview controller for epay page

---
+++ MidtransKit/MidtransKit/VTEpayPageController.h
@@ -0,0 +1,13 @@
+//
+//  VTEpayPageController.h
+//  MidtransKit
+//
+//  Created by Nanang Rafsanjani on 6/14/16.
+//  Copyright © 2016 Veritrans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VTEpayPageController : UIViewController
+- (instancetype _Nonnull)initWithRedirectURL:(NSURL * _Nonnull)redirectURL;
+@end

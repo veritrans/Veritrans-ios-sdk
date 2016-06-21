866ec8c3465615f112c9bd055989990985e600c2
vanbungkring <prasetyo.mailbox@gmail.com>
2016-06-21 13:40:39 +0700

    handle multiple bank account in one page

--- MidtransKit/MidtransKit/VTPaymentDirectViewController.m
+++ MidtransKit/MidtransKit/VTPaymentDirectViewController.m
@@ -10,9 +10,13 @@
 #import "VTPaymentDirectView.h"
 #import "VTTextField.h"
 #import "VTButton.h"
+#import "VTVATransactionStatusViewModel.h"
+#import "VTBillpaySuccessController.h"
+#import "VTVASuccessStatusController.h"
+#import <MidtransCoreKit/MidtransCoreKit.h>
 @interface VTPaymentDirectViewController ()
 @property (strong, nonatomic) IBOutlet VTPaymentDirectView *view;
-
+@property (nonatomic) VTVAType paymentType;
 @end
 
 @implementation VTPaymentDirectViewController
@@ -20,40 +24,78 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSMutableString *mutableString = [NSMutableString string];
-    [mutableString appendString:@"How can i Pay Via"];
     NSString *paymentName = @"";
     self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle: NSLocalizedString(@"Back", nil)
                                                                              style:UIBarButtonItemStylePlain
                                                                             target:nil
                                                                             action:nil];
     if ([self.paymentMethod.internalBaseClassIdentifier isEqualToString:VT_PAYMENT_INDOMARET]) {
-        paymentName  = @"Indomaret";
+        paymentName  = NSLocalizedString(@"Indomaret",nil);
     }
     else if ([self.paymentMethod.internalBaseClassIdentifier isEqualToString:VT_PAYMENT_KLIK_BCA]) {
-        paymentName  = @"KlikBCA";
+        paymentName  =  NSLocalizedString(@"KlikBCA",nil);
     }
     else if ([self.paymentMethod.internalBaseClassIdentifier isEqualToString:VT_VA_BCA_IDENTIFIER]) {
-        paymentName  = @"BCA ATM";
+        self.paymentType = VTVATypeBCA;
+        paymentName  =  NSLocalizedString(@"BCA ATM",nil);
     }
     else if ([self.paymentMethod.internalBaseClassIdentifier isEqualToString:VT_VA_MANDIRI_IDENTIFIER]) {
-        paymentName  = @"Mandiri ATM";
+        self.paymentType = VTVATypeMandiri;
+        paymentName  =  NSLocalizedString(@"Mandiri ATM",nil);
     }
     else if ([self.paymentMethod.internalBaseClassIdentifier isEqualToString:VT_VA_PERMATA_IDENTIFIER]) {
-        paymentName  = @"Permata ATM";
+        self.paymentType = VTVATypePermata;
+        paymentName  =  NSLocalizedString(@"Permata ATM",nil);
     }
     else if ([self.paymentMethod.internalBaseClassIdentifier isEqualToString:VT_VA_OTHER_IDENTIFIER]) {
-        paymentName  = @"Other Bank";
+        self.paymentType = VTVATypeOther;
+        paymentName  =  NSLocalizedString(@"Other Bank",nil);
     }
     
     self.title = [NSString stringWithFormat: NSLocalizedString(@"Pay at %@",nil),[paymentName capitalizedString]];
-    [self.view.howToPaymentButton setTitle:[NSString stringWithFormat:@"How can i Pay Via %@",[paymentName capitalizedString]] forState:UIControlStateNormal];
+    [self.view.howToPaymentButton setTitle:[NSString stringWithFormat:NSLocalizedString(@"How can i Pay Via %@",nil),paymentName] forState:UIControlStateNormal];
     self.view.totalAmountLabel.text = [[NSObject indonesianCurrencyFormatter] stringFromNumber:self.transactionDetails.grossAmount];
     self.view.orderIdLabel.text = self.transactionDetails.orderId;
     self.view.directPaymentTextField.text = self.customerDetails.email;
     // Do any additional setup after loading the view from its nib.
 }
-
+- (IBAction)paymentGuideDidTapped:(id)sender {
+    
+}
+- (IBAction)confirmPaymentDidTapped:(id)sender {
+    if ([self.paymentMethod.internalBaseClassIdentifier isEqualToString:VT_PAYMENT_KLIK_BCA] ||
+        [self.paymentMethod.internalBaseClassIdentifier isEqualToString:VT_VA_BCA_IDENTIFIER] ||
+        [self.paymentMethod.internalBaseClassIdentifier isEqualToString:VT_VA_MANDIRI_IDENTIFIER] ||
+        [self.paymentMethod.internalBaseClassIdentifier isEqualToString:VT_VA_PERMATA_IDENTIFIER] ||
+        [self.paymentMethod.internalBaseClassIdentifier isEqualToString:VT_VA_OTHER_IDENTIFIER]) {
+        [self showLoadingHud];
+        VTPaymentBankTransfer *paymentDetails = [[VTPaymentBankTransfer alloc] initWithBankTransferType:self.paymentType];
+        self.customerDetails.email = self.view.directPaymentTextField.text;
+        VTTransaction *transaction = [[VTTransaction alloc] initWithPaymentDetails:paymentDetails transactionDetails:self.transactionDetails customerDetails:self.customerDetails itemDetails:self.itemDetails];
+        [[VTMerchantClient sharedClient] performTransaction:transaction completion:^(VTTransactionResult *result, NSError *error) {
+            [self hideLoadingHud];
+            if (error) {
+                [self handleTransactionError:error];
+            } else {
+                [self handleTransactionSuccess:result];
+            }
+        }];
+        
+        
+    }
+    else if ([self.paymentMethod.internalBaseClassIdentifier isEqualToString:VT_PAYMENT_INDOMARET]) {
+    }
+}
+- (void)handleTransactionSuccess:(VTTransactionResult *)result {
+    VTVATransactionStatusViewModel *vm = [[VTVATransactionStatusViewModel alloc] initWithTransactionResult:result vaType:self.paymentType];
+    if (self.paymentType == VTVATypeMandiri) {
+        VTBillpaySuccessController *vc = [[VTBillpaySuccessController alloc] initWithViewModel:vm];
+        [self.navigationController pushViewController:vc animated:YES];
+    } else {
+        VTVASuccessStatusController *vc = [[VTVASuccessStatusController alloc] initWithViewModel:vm];
+        [self.navigationController pushViewController:vc animated:YES];
+    }
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.

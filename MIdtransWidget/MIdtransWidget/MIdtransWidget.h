7991052e6b22f463fc6aeb0415c520caf3ee059e
vanbungkring <prasetyo.mailbox@gmail.com>
2016-09-21 14:04:03 +0700

    add generic widget for midtranskit

---
+++ MIdtransWidget/MIdtransWidget/MIdtransWidget.h
@@ -0,0 +1,13 @@
+//
+//  MIdtransWidget.h
+//  MIdtransWidget
+//
+//  Created by Arie on 9/21/16.
+//  Copyright © 2016 Arie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MIdtransWidget : NSObject
+
+@end
---
+++ MIdtransWidget/MIdtransWidget/MIdtransWidget.m
@@ -0,0 +1,13 @@
+//
+//  MIdtransWidget.m
+//  MIdtransWidget
+//
+//  Created by Arie on 9/21/16.
+//  Copyright © 2016 Arie. All rights reserved.
+//
+
+#import "MIdtransWidget.h"
+
+@implementation MIdtransWidget
+
+@end

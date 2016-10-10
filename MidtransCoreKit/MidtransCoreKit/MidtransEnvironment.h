f6a760e8deb528ce73acfedd0eeae03997afc243
vanbungkring <prasetyo.mailbox@gmail.com>
2016-10-10 14:59:05 +0700

    update sdk version to 1.0, fix tracking manager

--- MidtransCoreKit/MidtransCoreKit/MidtransEnvironment.h
+++ MidtransCoreKit/MidtransCoreKit/MidtransEnvironment.h
@@ -6,7 +6,11 @@
 //  Copyright © 2016 Veritrans. All rights reserved.
 //
 
+<<<<<<< Updated upstream
 #define VERSION @"0.1"
+=======
+#define VERSION @"1.0.0"
+>>>>>>> Stashed changes
 
 /**
  The payment server types.

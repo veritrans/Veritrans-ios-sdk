8fc789390f07c4de0802ccb6618aac0f6e5a3164
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2016-10-12 09:44:39 +0700

    set VTCardListController as public class

--- MidtransKit/MidtransKit/MidtransKit.h
+++ MidtransKit/MidtransKit/MidtransKit.h
@@ -12,5 +12,11 @@
 #import "VTCardControllerConfig.h"
 #import "VTPaymentViewController.h"
 #import "VTCardControllerConfig.h"
+#import "VTPaymentStatusViewModel.h"
+#import "VTCardListController.h"
+#import "VTSuccessStatusController.h"
+#import "VTErrorStatusController.h"
+#import "VTPaymentViewController.h"
+#import "VTPaymentController.h"
 
 #endif /* MidtransKit_h */

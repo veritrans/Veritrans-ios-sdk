6631452e9d269a8893f2f77249e44cf11c848cef
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2016-11-23 17:32:46 +0700

    refine cc payment object

--- MidtransCoreKit/MidtransCoreKit/MidtransPaymentCreditCard.m
+++ MidtransCoreKit/MidtransCoreKit/MidtransPaymentCreditCard.m
@@ -21,36 +21,28 @@ typedef NS_ENUM(NSUInteger, MidtransPaymentCreditCardType) {
 @property (nonatomic) NSString *_Nonnull creditCardToken;
 @property (nonatomic) MidtransCustomerDetails *customerDetails;
 @property (nonatomic) NSString *maskedCard;
-@property (nonatomic, assign) MidtransPaymentCreditCardType paymentType;
+@property (nonatomic) BOOL saveCard;
 @end
 
 @implementation MidtransPaymentCreditCard
 
-+ (instancetype)paymentOneClickWithMaskedCard:(NSString *)maskedCard customer:(MidtransCustomerDetails *)customer {
++ (instancetype)modelWithToken:(NSString *)token customer:(MidtransCustomerDetails *)customer saveCard:(BOOL)saveCard {
+    MidtransPaymentCreditCard *payment = [MidtransPaymentCreditCard new];
+    payment.customerDetails = customer;
+    payment.creditCardToken = token;
+    payment.saveCard = saveCard;
+    return payment;
+}
+
++ (instancetype)modelWithMaskedCard:(NSString *)maskedCard customer:(MidtransCustomerDetails *)customer saveCard:(BOOL)saveCard {
     MidtransPaymentCreditCard *payment = [MidtransPaymentCreditCard new];
     payment.customerDetails = customer;
     payment.maskedCard = maskedCard;
-    payment.paymentType = MidtransPaymentCreditCardTypeOneclick;
+    payment.saveCard = saveCard;
     return payment;
 }
 
-+ (instancetype)paymentTwoClicksWithToken:(NSString *)token customer:(MidtransCustomerDetails *)customer {
-    MidtransPaymentCreditCard *payment = [MidtransPaymentCreditCard new];
-    payment.customerDetails = customer;
-    payment.creditCardToken = token;
-    payment.paymentType = MidtransPaymentCreditCardTypeTwoClicks;
-    return payment;
-}
-
-+ (instancetype)paymentWithToken:(NSString *)token customer:(MidtransCustomerDetails *)customer {
-    MidtransPaymentCreditCard *payment = [MidtransPaymentCreditCard new];
-    payment.customerDetails = customer;
-    payment.creditCardToken = token;
-    payment.paymentType = MidtransPaymentCreditCardTypeNormal;
-    return payment;
-}
-
-- (NSDictionary *)dictionaryValue {    
+- (NSDictionary *)dictionaryValue {
     return @{@"payment_type":MIDTRANS_PAYMENT_CREDIT_CARD,
              @"payment_params":[self paymentParameter],
              @"customer_details":@{@"email":self.customerDetails.email,
@@ -60,18 +52,12 @@ typedef NS_ENUM(NSUInteger, MidtransPaymentCreditCardType) {
 
 - (NSDictionary *)paymentParameter {
     NSMutableDictionary *parameters = [NSMutableDictionary new];
-    
-    switch (self.paymentType) {
-        case MidtransPaymentCreditCardTypeNormal:
-            [parameters setObject:self.creditCardToken forKey:@"card_token"];
-            [parameters setObject:@([CC_CONFIG saveCardEnabled]) forKey:@"save_card"];
-            break;
-        case MidtransPaymentCreditCardTypeTwoClicks:
-            [parameters setObject:self.creditCardToken forKey:@"card_token"];
-            break;
-        case MidtransPaymentCreditCardTypeOneclick:
-            [parameters setObject:self.maskedCard forKey:@"masked_card"];
-            break;
+    if (self.maskedCard) {
+        [parameters setObject:self.maskedCard forKey:@"masked_card"];
+    }
+    else {
+        [parameters setObject:self.creditCardToken forKey:@"card_token"];
+        [parameters setObject:@(self.saveCard) forKey:@"save_card"];
     }
     return parameters;
 }

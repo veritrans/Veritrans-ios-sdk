ffc67d29e223f02d225852270c981568120524d1
Arie Prasetyo <prasetyo.mailbox@gmail.com>
2017-01-20 03:53:00 +0700

    Separate SaveCard View to MidtransUISaveCardView

---
+++ MidtransKit/MidtransKit/MidtransUISaveCardView.h
@@ -0,0 +1,16 @@
+//
+//  MidtransUISaveCardView.h
+//  MidtransKit
+//
+//  Created by Vanbungkring on 1/20/17.
+//  Copyright © 2017 Veritrans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MidtransUISaveCardView : UIView
+@property (unsafe_unretained, nonatomic) IBOutlet UILabel *saveCardTextLabel;
+@property (unsafe_unretained, nonatomic) IBOutlet UIButton *saveCardInfoButton;
+@property (unsafe_unretained, nonatomic) IBOutlet UIButton *saveCardSelect;
+
+@end
---
+++ MidtransKit/MidtransKit/MidtransUISaveCardView.m
@@ -0,0 +1,21 @@
+//
+//  MidtransUISaveCardView.m
+//  MidtransKit
+//
+//  Created by Vanbungkring on 1/20/17.
+//  Copyright © 2017 Veritrans. All rights reserved.
+//
+
+#import "MidtransUISaveCardView.h"
+
+@implementation MidtransUISaveCardView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

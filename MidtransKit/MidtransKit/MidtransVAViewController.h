f2fe263fa36082355520a5d693c79bd5ab950853
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-03-01 13:26:39 +0700

    create new va page

---
+++ MidtransKit/MidtransKit/MidtransVAViewController.h
@@ -0,0 +1,13 @@
+//
+//  MidtransVAViewController.h
+//  MidtransKit
+//
+//  Created by Nanang Rafsanjani on 2/28/17.
+//  Copyright © 2017 Midtrans. All rights reserved.
+//
+
+#import "MidtransUIPaymentController.h"
+
+@interface MidtransVAViewController : MidtransUIPaymentController
+
+@end

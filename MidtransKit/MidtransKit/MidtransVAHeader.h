5a184f917dddcaeea483a1495f0cc532dc8f7532
Nanang Rafsanjani <jukiginanjar@users.noreply.github.com>
2017-03-01 15:28:10 +0700

    Feature/refine va layout (#208)
    * create new va page
    
    * update va list to call new va page
    
    * add new localized string for pay va button
    
    * update demo and schemes

---
+++ MidtransKit/MidtransKit/MidtransVAHeader.h
@@ -0,0 +1,16 @@
+//
+//  MidtransVAHeader.h
+//  MidtransKit
+//
+//  Created by Nanang Rafsanjani on 2/28/17.
+//  Copyright © 2017 Midtrans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MidtransUITextField.h"
+
+@interface MidtransVAHeader : UITableViewCell
+@property (strong, nonatomic) IBOutlet MidtransUITextField *emailTextField;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *tabSwitch;
+@property (strong, nonatomic) IBOutlet UILabel *descLabel;
+@end

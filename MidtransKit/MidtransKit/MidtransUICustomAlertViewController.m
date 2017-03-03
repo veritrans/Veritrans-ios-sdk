9628f03a201b2dd9f69381089280b5815213930c
Arie Prasetyo <arie.prasetyo@midtrans.com>
2017-03-03 15:15:58 +0700

    fix alert dismiss

--- MidtransKit/MidtransKit/MidtransUICustomAlertViewController.m
+++ MidtransKit/MidtransKit/MidtransUICustomAlertViewController.m
@@ -105,8 +105,8 @@
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         if ([self.delegate respondsToSelector:@selector(didSelectOKButtonAlertViewController:)]) {
             [self.delegate didSelectOKButtonAlertViewController:self];
-            [self dismissCustomViewController:nil];
         }
+          [self dismissCustomViewController:nil];
     });
 }
 

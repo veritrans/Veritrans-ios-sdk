b2e06fe3c6ecda2f1eb2b4bcfbf77716310a8dfa
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-03-20 16:45:17 +0700

    refine travis config

--- scripts/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd DemoTest
-pod install
-rm -rf "~/Library/Caches/CocoaPods"
-rm -rf "Pods/"
-pod update
-xcodebuild -workspace VTDirectDemo.xcworkspace -scheme MidtransSDKDemo -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.0' ONLY_ACTIVE_ARCH=NO clean test | xcpretty

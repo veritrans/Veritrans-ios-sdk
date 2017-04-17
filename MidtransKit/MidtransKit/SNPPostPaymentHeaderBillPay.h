55d522789359c11f4cfc662f5b4c2d94ef2decb0
vanbungkring <prasetyo.mailbox@gmail.com>
2017-04-17 23:19:30 +0700

    add SNPHandleBillpayment

---
+++ MidtransKit/MidtransKit/SNPPostPaymentHeaderBillPay.h
@@ -0,0 +1,13 @@
+//
+//  SNPPostPaymentHeaderBillPay.h
+//  MidtransKit
+//
+//  Created by Vanbungkring on 4/17/17.
+//  Copyright © 2017 Midtrans. All rights reserved.
+//
+
+#import "SNPPostPaymentHeader.h"
+
+@interface SNPPostPaymentHeaderBillPay : SNPPostPaymentHeader
+
+@end
---
+++ MidtransKit/MidtransKit/SNPPostPaymentHeaderBillPay.m
@@ -0,0 +1,21 @@
+//
+//  SNPPostPaymentHeaderBillPay.m
+//  MidtransKit
+//
+//  Created by Vanbungkring on 4/17/17.
+//  Copyright © 2017 Midtrans. All rights reserved.
+//
+
+#import "SNPPostPaymentHeaderBillPay.h"
+
+@implementation SNPPostPaymentHeaderBillPay
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

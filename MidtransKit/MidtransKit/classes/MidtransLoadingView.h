518f48c627d67af0652e9ea455bbb0b0ed3beb59
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-04-26 13:50:28 +0700

    refine loading indicator

--- MidtransKit/MidtransKit/classes/MidtransLoadingView.h
+++ MidtransKit/MidtransKit/classes/MidtransLoadingView.h
@@ -9,10 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface MidtransLoadingView : UIView
-@property (weak, nonatomic) IBOutlet UILabel *loadingTitleLabel;
-- (void)show;
-- (void)showWithTitle:(NSString *)title;
+
 - (void)hide;
-- (void)remove;
 - (void)showInView:(UIView *)view withText:(NSString *)text;
 @end

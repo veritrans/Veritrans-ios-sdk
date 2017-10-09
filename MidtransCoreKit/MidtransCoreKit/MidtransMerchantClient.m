5d50353f11bfd82259065ac7c5bb29eb8358d0e4
vanbungkring <prasetyo.mailbox@gmail.com>
2017-10-09 14:38:03 +0700

    update creditcard config when handle nil

--- MidtransCoreKit/MidtransCoreKit/MidtransMerchantClient.m
+++ MidtransCoreKit/MidtransCoreKit/MidtransMerchantClient.m
@@ -228,8 +228,8 @@ NSString *const FETCH_MASKEDCARD_URL = @"%@/users/%@/tokens";
     if (CC_CONFIG.acquiringBankString) {
         creditCardParameter[@"bank"] = CC_CONFIG.acquiringBankString;
     }
-    if ([CC_CONFIG.authenticationTypeString length]>0) {
-        creditCardParameter[@"authentication"] = CC_CONFIG.authenticationTypeString?CC_CONFIG.authenticationTypeString:@"none";
+    if (CC_CONFIG.authenticationTypeString!=nil || [CC_CONFIG.authenticationTypeString length]>0) {
+        creditCardParameter[@"authentication"] = CC_CONFIG.authenticationTypeString;
     }
     if (CC_CONFIG.preauthEnabled) {
         creditCardParameter[@"type"] = @"authorize";

b5ac3643e5d1c15b56082731024cdacf34e8b4cc
tommyyohanesmt <31880480+tommyyohanesmt@users.noreply.github.com>
2017-11-03 11:59:10 +0700

    Feature/mob 1266 primarybutton (#322)
    * add detail disclosure button
    
    * added margin and borders to primary buttons
    
    * add corner radius on midtrans nextstep button
    
    * delete constraint to prevent conflict with dev
    
    * delete top border line view above confirm button
    
    * delete header view to prevent conflict with dev
    
    * delete constraint button with scroll view
    
    * resolve conflict
    
    * do the same thing for snppointviewcontroller
    
    * fix ui on new credit card & snp point view controller
    
    * fix  confirm button height on new credit card

--- MidtransKit/MidtransKit/classes/MidtransNewCreditCardView.h
+++ MidtransKit/MidtransKit/classes/MidtransNewCreditCardView.h
@@ -10,7 +10,7 @@
 @class MidtransUITextField;
 @class MidtransTransactionTokenResponse,MidtransUINextStepButton,MidtransPaymentMethodHeader;
 @interface MidtransNewCreditCardView : UIView
-@property (weak, nonatomic) IBOutlet UIButton *scanCardButton;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet MidtransUITextField *creditCardNumberTextField;
 @property (weak, nonatomic) IBOutlet MidtransUITextField *cardExpireTextField;
 @property (weak, nonatomic) IBOutlet MidtransUITextField *cardCVVNumberTextField;

f3437c6288d149a6b57a459ea5e4383290a07a2f
vanbungkring <prasetyo.mailbox@gmail.com>
2018-04-06 17:30:49 +0700

    update payment order

--- MidtransDemo/MidtransDemo/MDOrderViewController.m
+++ MidtransDemo/MidtransDemo/MDOrderViewController.m
@@ -202,7 +202,7 @@
          }
          else {
 
-             MidtransUIPaymentViewController *paymentVC = [[MidtransUIPaymentViewController alloc] init];
+             MidtransUIPaymentViewController *paymentVC = [[MidtransUIPaymentViewController alloc] initWithToken:token];
              paymentVC.paymentDelegate = self;
              [self.navigationController presentViewController:paymentVC animated:YES completion:nil];
          }

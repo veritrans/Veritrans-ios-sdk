63582765e7a4914dddbe587a6a5a1a65b1b340d1
tommyyohanesmt <tommy.yohanes@midtrans.com>
2018-05-21 11:29:37 +0700

    self explanatory

--- MidtransDemo/MidtransDemo/MDOrderViewController.m
+++ MidtransDemo/MidtransDemo/MDOrderViewController.m
@@ -66,9 +66,9 @@
             merchantServer = @"https://demo-merchant-server.herokuapp.com";
             break;
     }
-        [CONFIG setClientKey:@"VT-client-E4f1bsi1LpL1p5cF"
-                 environment:MidtransServerEnvironmentSandbox
-           merchantServerURL:@"https://rakawm-snap.herokuapp.com/installment/"];
+    [CONFIG setClientKey:clientkey
+             environment:MidtransServerEnvironmentSandbox
+       merchantServerURL:merchantServer];
     
     //forced to use token storage
     UICONFIG.hideStatusPage = YES;

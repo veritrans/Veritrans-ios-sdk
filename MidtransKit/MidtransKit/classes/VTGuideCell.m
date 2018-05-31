10d9bc594821f61770761128ce246bad503d5cd0
tommyyohanesmt <tommy.yohanes@midtrans.com>
2018-05-31 15:15:10 +0700

    correct klick bca guide and make User ID bold

--- MidtransKit/MidtransKit/classes/VTGuideCell.m
+++ MidtransKit/MidtransKit/classes/VTGuideCell.m
@@ -122,16 +122,26 @@
        [attrString endEditing];
        self.contentLabel.attributedText = attrString;
    }
-    else if ([instruction.content containsString:@"Correct"]) {
-        NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:instruction.content];
-        [attrString beginEditing];
-        [attrString addAttribute:NSFontAttributeName
-                           value:[UIFont fontWithName:FONT_NAME_BOLD size:12.0]
-                           range:[attrString.string rangeOfString:@"Correct"]];
-        
-        [attrString endEditing];
-        self.contentLabel.attributedText = attrString;
-    }
+   else if ([instruction.content containsString:@"Correct"]) {
+       NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:instruction.content];
+       [attrString beginEditing];
+       [attrString addAttribute:NSFontAttributeName
+                          value:[UIFont fontWithName:FONT_NAME_BOLD size:12.0]
+                          range:[attrString.string rangeOfString:@"Correct"]];
+       
+       [attrString endEditing];
+       self.contentLabel.attributedText = attrString;
+   }
+   else if ([instruction.content containsString:@"User ID"]) {
+       NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:instruction.content];
+       [attrString beginEditing];
+       [attrString addAttribute:NSFontAttributeName
+                          value:[UIFont fontWithName:FONT_NAME_BOLD size:12.0]
+                          range:[attrString.string rangeOfString:@"User ID"]];
+       
+       [attrString endEditing];
+       self.contentLabel.attributedText = attrString;
+   }
     else if ([instruction.content containsString:@"Transfer"]) {
         NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:instruction.content];
         [attrString beginEditing];

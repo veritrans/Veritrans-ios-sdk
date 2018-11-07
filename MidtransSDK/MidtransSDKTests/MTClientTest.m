2e4fe12e94be4cd99a076e05e86fc28203e28662
Nanang Rafsanjani <jukiginanjar@hotmail.com>
2018-11-07 15:38:31 +0700

    init test

---
+++ MidtransSDK/MidtransSDKTests/MTClientTest.m
@@ -0,0 +1,37 @@
+//
+//  MTClientTest.m
+//  MidtransSDKTests
+//
+//  Created by Nanang Rafsanjani on 07/11/18.
+//  Copyright © 2018 Midtrans. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "MTClient.h"
+
+@interface MTClientTest : XCTestCase
+
+@end
+
+@implementation MTClientTest
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testInitialize {
+    [[MTClient shared] configureClientKey:nil environment:MTEnvironmentSandbox merchantServerURL:nil];
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

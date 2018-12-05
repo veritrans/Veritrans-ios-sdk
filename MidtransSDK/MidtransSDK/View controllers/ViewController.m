e3bac829f51730589a0ea70ce0f42946b043f018
Nanang Rafsanjani <jukiginanjar@hotmail.com>
2018-12-05 16:21:45 +0700

    update demo

--- MidtransSDK/MidtransSDK/View controllers/ViewController.m
+++ MidtransSDK/MidtransSDK/View controllers/ViewController.m
@@ -23,39 +23,13 @@
 }
 
 - (IBAction)checkoutPressed:(id)sender {
-//    NSDictionary *terms = @{@"bni": @[@3, @6, @12],
-//                            @"mandiri": @[@3, @6],
-//                            @"cimb": @[@3],
-//                            @"offline": @[@6, @12]
-//                            };
-//    NSArray *whitelistBins = @[@"48111111",
-//                               @"41111111",
-//                               @"bni"
-//                               ];
-//    MIDCheckoutInstallment *installment = [[MIDCheckoutInstallment alloc] initWithTerms:terms required:NO];
-//    MIDCheckoutCreditCard *cc = [[MIDCheckoutCreditCard alloc] initWithTransactionType:MIDCreditCardTransactionTypeAuthorizeCapture
-//                                                                          enableSecure:NO
-//                                                                        enableSaveCard:NO
-//                                                                         acquiringBank:MIDAcquiringBankNone
-//                                                                      acquiringChannel:MIDAcquiringChannelNone
-//                                                                           installment:installment
-//                                                                         whiteListBins:whitelistBins];
-//
-//    MIDCheckoutCustomer *customer = [[MIDCheckoutCustomer alloc] initWithFirstName:@"Nanang"
-//                                                                          lastName:@"Rafsanjani"
-//                                                                             email:nil
-//                                                                             phone:nil
-//                                                                    billingAddress:nil
-//                                                                   shippingAddress:nil];
-    
-    MIDCheckoutGoPay *gopay = [[MIDCheckoutGoPay alloc] initWithCallbackSchemeURL:@"demo.midtrans"];
-    
     NSDate *date = [NSDate new];
     NSString *orderID = [NSString stringWithFormat:@"%f", date.timeIntervalSince1970];
     MIDCheckoutTransaction *trx = [[MIDCheckoutTransaction alloc] initWithOrderID:orderID grossAmount:@1000];
     
-    [[MIDClient shared] checkoutWith:trx options:@[gopay] completion:^(MIDToken * _Nullable token, NSError * _Nullable error) {
-//        NSLog(@"Token: %@", token.dictionaryValue);
+    
+    [MIDClient checkoutWith:trx options:nil completion:^(MIDToken * _Nullable token, NSError * _Nullable error) {
+        NSLog(@"Token: %@", token.dictionaryValue);
         
         [self fetchPaymentInfo:token.token];
         
@@ -64,31 +38,18 @@
 }
 
 - (void)fetchPaymentInfo:(NSString *)token {
-    [[MIDClient shared] getPaymentInfoWithToken:token completion:^(MIDPaymentInfo * _Nullable info, NSError * _Nullable error) {
-//        NSLog(@"Payment info: %@", info.dictionaryValue);
+    [MIDClient getPaymentInfoWithToken:token completion:^(MIDPaymentInfo * _Nullable info, NSError * _Nullable error) {
+        NSLog(@"Payment info: %@", info.dictionaryValue);
     }];
 }
 
 - (void)payWithToken:(NSString *)token {
-//    MIDVirtualAccountPayment *payment = [[MIDVirtualAccountPayment alloc] initWithType:MIDVirtualAccountTypeBCA email:nil];
-//    MIDVirtualAccountPayment *payment = [[MIDVirtualAccountPayment alloc] initWithType:MIDVirtualAccountTypeEchannel email:nil];
-//    MIDVirtualAccountPayment *payment = [[MIDVirtualAccountPayment alloc] initWithType:MIDVirtualAccountTypePermata email:nil];
-//    MIDVirtualAccountPayment *payment = [[MIDVirtualAccountPayment alloc] initWithType:MIDVirtualAccountTypeBNI email:nil];
-    
-//    MIDOnlinePayment *payment = [[MIDOnlinePayment alloc] initWithType:MIDOnlinePaymentTypeBRIEpay];
-//    MIDOnlinePayment *payment = [[MIDOnlinePayment alloc] initWithType:MIDOnlinePaymentTypeBCAKlikPay];
-//    MIDOnlinePayment *payment = [[MIDOnlinePayment alloc] initWithType:MIDOnlinePaymentTypeCIMBClicks];
-//    MIDOnlinePayment *payment = [[MIDOnlinePayment alloc] initWithType:MIDOnlinePaymentTypeMandiriEcash];
-//    MIDOnlinePayment *payment = [[MIDOnlinePayment alloc] initWithType:MIDOnlinePaymentTypeDanamonOnline];
-//    MIDOnlinePayment *payment = [[MIDOnlinePayment alloc] initWithType:MIDOnlinePaymentTypeAkulaku];
-    
-//    MIDGoPayPayment *payment = [MIDGoPayPayment new];
-    
-    MIDIndomaretPayment *payment = [MIDIndomaretPayment new];
-    
-    [[MIDClient shared] performPayment:payment token:token completion:^(MIDPaymentResult * _Nullable result, NSError * _Nullable error) {
-        NSLog(@"Payment result: %@", result.dictionaryValue);
-    }];
+    [MIDEWalletCharge tcashWithToken:token
+                         phoneNumber:@"0811111111"
+                          completion:^(MIDPaymentResult * _Nullable result, NSError * _Nullable error)
+     {
+         NSLog(@"Charge result: %@", [result dictionaryValue]);
+     }];
 }
 
 @end

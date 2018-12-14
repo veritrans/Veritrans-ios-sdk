a037e058048c9128a20af765470a1d6b1a09c7d8
Nanang Rafsanjani <jukiginanjar@hotmail.com>
2018-12-14 14:11:09 +0700

    remove old SDK

--- Old/MidtransCoreKit/MidtransCoreKit/Gopay/MIDGOPAYDataModels.h
+++
@@ -1,8 +0,0 @@
-//
-//  DataModels.h
-//
-//  Created by Ratna Kumalasari on 11/28/17
-//  Copyright (c) 2017 __MyCompanyName__. All rights reserved.
-//
-
-#import "MIDGOPAYResponse.h"
--- Old/MidtransCoreKit/MidtransCoreKit/Promo/MidtransPromoDataModels.h
+++
@@ -1,8 +0,0 @@
-//
-//  DataModels.h
-//
-//  Created by Ratna Kumalasari on 3/28/18
-//  Copyright (c) 2018 __MyCompanyName__. All rights reserved.
-//
-
-#import "MidtransPromoPromoDetails.h"#import "MidtransPromoResponse.h"#import "MidtransPromoPromos.h"

971f325b515460e17e1f14b6df2b77ed6ed43c14
Nanang Rafsanjani <jukiginanjar@users.noreply.github.com>
2018-12-18 18:26:24 +0700

    remove old SDK (#408)

--- Old/MidtransCoreKit/MidtransCoreKit/MidtransPaymentListDataModel.h
+++
@@ -1,8 +0,0 @@
-//
-//  DataModels.h
-//
-//  Created by Arie  on 6/17/16
-//  Copyright (c) 2016 __MyCompanyName__. All rights reserved.
-//
-
-#import "MidtransPaymentListModel.h"
--- Old/MidtransCoreKit/MidtransCoreKit/SNPPointDataModels.h
+++
@@ -1,8 +0,0 @@
-//
-//  DataModels.h
-//
-//  Created by Zanna Simarmata on 3/7/17
-//  Copyright (c) 2017 __MyCompanyName__. All rights reserved.
-//
-
-#import "SNPPointResponse.h"

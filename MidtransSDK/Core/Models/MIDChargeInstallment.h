efa87a4511f53923b4edffa2c93f1be626da633a
Nanang Rafsanjani <jukiginanjar@hotmail.com>
2019-01-03 16:40:04 +0700

    add charge installment object

---
+++ MidtransSDK/Core/Models/MIDChargeInstallment.h
@@ -0,0 +1,22 @@
+//
+//  MIDCreditCardInstallment.h
+//  MidtransSDK
+//
+//  Created by Nanang Rafsanjani on 27/12/18.
+//  Copyright © 2018 Midtrans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MIDModelEnums.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MIDChargeInstallment : NSObject
+
++ (instancetype)modelWithBank:(MIDAcquiringBank)bank term:(NSInteger)term;
+
+- (NSString *)value;
+
+@end
+
+NS_ASSUME_NONNULL_END

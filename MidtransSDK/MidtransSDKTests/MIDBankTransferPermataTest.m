daba871230438b09485966f6e25f425112b8af77
Arie.Prasetiyo <arie>
2019-01-08 13:02:44 +0700

    update test for permata negativity test

---
+++ MidtransSDK/MidtransSDKTests/MIDBankTransferPermataTest.m
@@ -0,0 +1,67 @@
+//
+//  MIDBankTransferMandiriTest.m
+//  MidtransSDKTests
+//
+//  Created by Arie.Prasetiyo on 08/01/19.
+//  Copyright © 2019 Midtrans. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "MIDTestHelper.h"
+@interface MIDBankTransferPermataTest : XCTestCase
+
+@end
+
+@implementation MIDBankTransferPermataTest
+static NSString *_email = @"test-mobile@midtrans.com";
+
+- (void)setUp {
+    [MIDTestHelper setup];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testForPermata {
+    XCTestExpectation *promise = [XCTestExpectation new];
+    
+    [self getTokenWithCompletion:^(NSString * _Nullable token, NSError * _Nullable error) {
+        [MIDBankTransferCharge permataWithToken:token
+                                          email:_email
+                                     completion:^(MIDPermataBankTransferResult * _Nullable result, NSError * _Nullable error) {
+            XCTAssertNotNil(result.vaNumber, @"va bni test is error");
+            [promise fulfill];
+        }];
+    }];
+    
+    [self waitForExpectations:@[promise] timeout:120];
+}
+- (void)testTokenNotFoundPermata {
+    XCTestExpectation *promise = [XCTestExpectation new];
+    
+    [MIDBankTransferCharge permataWithToken:nil
+                                      email:_email
+                                 completion:^(MIDPermataBankTransferResult * _Nullable result, NSError * _Nullable error)
+     {
+         XCTAssertTrue(error.code == 404);
+         [promise fulfill];
+     }];
+    
+    [self waitForExpectations:@[promise] timeout:120];
+}
+
+- (void)getTokenWithCompletion:(void (^_Nullable) (NSString *_Nullable token, NSError *_Nullable error))completion {
+    MIDCheckoutTransaction *trx = [MIDCheckoutTransaction modelWithOrderID:[MIDTestHelper orderID]
+                                                               grossAmount:@1000000
+                                                                  currency:MIDCurrencyIDR];
+    
+    [MIDClient checkoutWith:trx options:nil completion:^(MIDToken * _Nullable token, NSError * _Nullable error) {
+        NSString *_token = token.token;
+        XCTAssertNotNil(_token);
+        completion(_token, error);
+    }];
+    
+}
+@end

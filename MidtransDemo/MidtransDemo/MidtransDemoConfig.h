58760b21a78145d54f4a7b177aa963b644331e24
Muhammad Fauzi Masykur <muhammad.masykur@go-jek.com>
2020-10-05 14:48:18 +0700

    create demo app config  for easy modification.

---
+++ MidtransDemo/MidtransDemo/MidtransDemoConfig.h
@@ -0,0 +1,28 @@
+//
+//  MidtransDemoConfig.h
+//  MidtransDemo
+//
+//  Created by Muhammad Fauzi Masykur on 05/10/20.
+//  Copyright © 2020 Midtrans. All rights reserved.
+//
+
+
+/**
+*  SANDBOX CREDENTIALS
+*/
+
+static NSString * const ONE_CLICK_MERCHANT_SERVER_URL_SANDBOX = @"https://fauzi-one-click-sandbox.herokuapp.com/";
+static NSString * const ONE_CLICK_MERCHANT_CLIENT_KEY_SANDBOX = @"SB-Mid-client-61XuGAwQ8Bj8LxSS";
+
+static NSString * const PROMO_MERCHANT_SERVER_URL_SANDBOX = @"https://promo-engine-sample-server.herokuapp.com/";
+static NSString * const PROMO_MERCHANT_CLIENT_KEY_SANDBOX = @"VT-client-yrHf-c8Sxr-ck8tx";
+
+static NSString * const FIESTAPOINT_MERCHANT_SERVER_URL_SANDBOX = @"https://fiesta-point-sample.herokuapp.com/";
+static NSString * const FIESTAPOINT_MERCHANT_CLIENT_KEY_SANDBOX = @"SB-Mid-server-zUQUBGT2BM07quQ9kV1G9nRt";
+
+/**
+*  ITEM CONFIGURATION
+*/
+
+static int const DEMO_APP_ITEM_PRICE = 150000;
+

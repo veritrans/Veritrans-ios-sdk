583d841c2bae6d7bb31cedac47c494692ef34c8a
Muhammad Fauzi Masykur <muhammad.masykur@go-jek.com>
2020-11-03 17:41:18 +0700

    fix title on shopeePay instructions page

--- MidtransKit/MidtransKit/MidShopeePayViewController.m
+++ MidtransKit/MidtransKit/MidShopeePayViewController.m
@@ -61,7 +61,7 @@
                                              selector:@selector(handleShopeePayStatus:)
                                                  name:UIApplicationDidBecomeActiveNotification
                                                object:nil];
-    self.title = @"ShopeePay";
+    self.title = self.paymentMethod.title;
     self.view.tableView.delegate = self;
     self.view.tableView.dataSource = self;
     self.view.tableView.tableFooterView = [UIView new];

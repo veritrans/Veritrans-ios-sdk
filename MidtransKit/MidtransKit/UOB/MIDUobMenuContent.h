141def29c4b3e0dd0e2f1cbb806044634478eb43
Muhammad Fauzi Masykur <muhammad.masykur@go-jek.com>
2021-06-15 01:33:47 +0700

    create model for uob menu options

---
+++ MidtransKit/MidtransKit/UOB/MIDUobMenuContent.h
@@ -0,0 +1,28 @@
+//
+//  MIDUobMenuModel.h
+//  MidtransKit
+//
+//  Created by Muhammad Fauzi Masykur on 14/06/21.
+//  Copyright © 2021 Midtrans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MIDUobMenuContent : NSObject
+
+@property (nonatomic, readonly) NSString *menuTitle;
+@property (nonatomic, readonly) NSString *menuDescription;
+@property (nonatomic, readonly) NSString *selectedTitle;
+@property (nonatomic, readonly) NSString *selectedOptions;
+
+- (instancetype)initWithMenuTitle:(NSString *)menuTitle
+                  menuDescription:(NSString *)menuDescription
+                    selectedTitle:(NSString *)selectedTitle
+                  selectedOption:(NSString *)selectedOptions;
+- (instancetype)initWithAppMenu;
+- (instancetype)initWithWebMenu;
+@end
+
+NS_ASSUME_NONNULL_END

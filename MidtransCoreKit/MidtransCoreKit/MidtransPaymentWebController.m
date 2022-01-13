13ea90aff71387426ceddcd05250db407e4cdcde
Muhammad Fauzi Masykur <muhammadfauzimasykur@gmail.com>
2022-01-13 14:54:31 +0700

    fix ios 15 ui issues on the webview

--- MidtransCoreKit/MidtransCoreKit/MidtransPaymentWebController.m
+++ MidtransCoreKit/MidtransCoreKit/MidtransPaymentWebController.m
@@ -64,7 +64,9 @@
     self.webView.navigationDelegate = self;
     
     self.view.backgroundColor = [UIColor whiteColor];
-    
+    if (@available(iOS 13.0, *)) {
+        self.navigationController.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
+    }
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemStop target:self action:@selector(closePressed:)];
     
     [self.view addSubview:self.webView];

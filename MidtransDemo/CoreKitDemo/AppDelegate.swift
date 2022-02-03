adec9ffa44448ff7512fa2ed348dfd426b65c7be
Muhammad Fauzi Masykur <muhammadfauzimasykur@gmail.com>
2022-02-03 09:33:47 +0700

    setup midtrans config

--- MidtransDemo/CoreKitDemo/AppDelegate.swift
+++ MidtransDemo/CoreKitDemo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MidtransCoreKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        MidtransConfig.shared().setClientKey("SB-Mid-client-zt7XrRxPQXZNvuBY", environment: .sandbox, merchantServerURL: "https://charmenzy-mid-mobile-sandbox.herokuapp.com/")
         return true
     }
 

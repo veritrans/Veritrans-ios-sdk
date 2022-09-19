6b01843bd24c9431ac905afd74c1b58a16cbc643
Muhammad Fauzi Masykur <muhammadfauzimasykur@gmail.com>
2022-09-19 15:14:52 +0700

    remove pragma mark

--- Package.swift
+++ Package.swift
@@ -18,12 +18,6 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        
-        //MARK:For Development
-//        .binaryTarget(name: "MidtransCoreKit", path: "MidtransCoreKit/MidtransCoreKit.xcframework"),
-//        .binaryTarget(name: "MidtransKit", path: "MidtransKit/MidtransKit.xcframework")
-        
-//        MARK:For Production
         .binaryTarget(name: "MidtransCoreKit", url: "https://github.com/uziwuzzy/Midtrans-xcframework/releases/download/1.23.0/MidtransCoreKit.xcframework-1.23.0.zip", checksum: "131f01b1c3292da27656dcfb176bc52c6f72eceb58cc7cdfbee52edaffb3924b"),
         .binaryTarget(name: "MidtransKit", url: "https://github.com/uziwuzzy/Midtrans-xcframework/releases/download/1.23.0/MidtransKit.xcframework-1.23.0.zip", checksum: "1f4e9339da762a47b71ac8dcd6b719ca6076bb5ad529732772786e421ab0b8b3")
     ]

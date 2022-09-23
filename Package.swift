f7e0530f1e470c49adeee7f0f65358899ef449ea
Muhammad Fauzi Masykur <muhammadfauzimasykur@gmail.com>
2022-09-23 10:10:27 +0700

    update xcframework host url

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "MidtransCoreKit", url: "https://github.com/uziwuzzy/Midtrans-xcframework/releases/download/1.23.0/MidtransCoreKit.xcframework-1.23.0.zip", checksum: "131f01b1c3292da27656dcfb176bc52c6f72eceb58cc7cdfbee52edaffb3924b"),
-        .binaryTarget(name: "MidtransKit", url: "https://github.com/uziwuzzy/Midtrans-xcframework/releases/download/1.23.0/MidtransKit.xcframework-1.23.0.zip", checksum: "1f4e9339da762a47b71ac8dcd6b719ca6076bb5ad529732772786e421ab0b8b3")
+        .binaryTarget(name: "MidtransCoreKit", url: "https://github.com/veritrans/Midtrans-xcframework/releases/download/1.23.0/MidtransCoreKit.xcframework-1.23.0.zip", checksum: "131f01b1c3292da27656dcfb176bc52c6f72eceb58cc7cdfbee52edaffb3924b"),
+        .binaryTarget(name: "MidtransKit", url: "https://github.com/veritrans/Midtrans-xcframework/releases/download/1.23.0/MidtransKit.xcframework-1.23.0.zip", checksum: "1f4e9339da762a47b71ac8dcd6b719ca6076bb5ad529732772786e421ab0b8b3")
     ]
 )

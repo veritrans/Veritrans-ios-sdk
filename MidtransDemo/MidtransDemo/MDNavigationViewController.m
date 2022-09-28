ce0e350c5e411b926aea27bae8986b78b2b3a9be
Muhammad Fauzi Masykur <muhammadfauzimasykur@gmail.com>
2022-09-28 11:29:04 +0700

    fix ios 15 navigation bar issue on demo app

--- MidtransDemo/MidtransDemo/MDNavigationViewController.m
+++ MidtransDemo/MidtransDemo/MDNavigationViewController.m
@@ -16,22 +16,47 @@
 
 @implementation MDNavigationViewController
 
+static NSInteger const MIDTRANS_DEMO_PAYMENT_STATUS_BAR_TAG = 1001;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.navigationBar.accessibilityIdentifier = @"demo_navbar";
-    
-    self.navigationBar.titleTextAttributes = @{
-                                               NSFontAttributeName:[UIFont bariolRegularWithSize:21],
-                                               NSForegroundColorAttributeName:[UIColor mdDarkColor]
-                                               };
-    self.navigationBar.translucent = NO;
-    self.navigationBar.barTintColor = [UIColor whiteColor];
-    self.navigationBar.tintColor = [UIColor mdThemeColor];
-    
+    [self setupNavigationBar];
     defaults_observe_object(@"md_color", ^(NSNotification *note) {
         self.navigationBar.tintColor = [UIColor mdThemeColor];
     });
+}
+
+- (void)setupNavigationBar {
+    self.navigationBar.translucent = NO;
+    [self.navigationBar setBackgroundImage:[UIImage new]
+                            forBarPosition:UIBarPositionAny
+                                barMetrics:UIBarMetricsDefault];
+    [self.navigationBar setBackgroundImage:[[UIImage alloc] init] forBarMetrics:UIBarMetricsDefault];
+    [self.navigationBar setShadowImage:[[UIImage alloc] init]];
+    self.navigationBar.tintColor = [[MidtransUIThemeManager shared] themeColor];
+    
+    if (@available(iOS 15.0, *)) {
+        self.navigationBar.backgroundColor = [UIColor whiteColor];
+        self.navigationBar.titleTextAttributes = @{NSFontAttributeName:[[MidtransUIThemeManager shared].themeFont fontRegularWithSize:17],
+                                                   NSForegroundColorAttributeName:[UIColor colorWithRed:3/255. green:3/255. blue:3/255. alpha:1]};
+        UIView * statusBarView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, [[UIApplication sharedApplication] statusBarFrame].size.height)];
+        statusBarView.backgroundColor = [UIColor whiteColor];
+        statusBarView.tag = MIDTRANS_DEMO_PAYMENT_STATUS_BAR_TAG;
+        [self.view addSubview:statusBarView];
+    } else {
+        self.navigationBar.titleTextAttributes = @{NSFontAttributeName:[[MidtransUIThemeManager shared].themeFont fontRegularWithSize:17],
+                                                   NSForegroundColorAttributeName:[UIColor colorWithRed:3/255. green:3/255. blue:3/255. alpha:1]};
+        self.navigationBar.barTintColor = [UIColor whiteColor];
+    }
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    if (@available(iOS 13.0, *)) {
+        return UIStatusBarStyleDarkContent;
+    } else {
+        return UIStatusBarStyleDefault;
+    }
 }
 
 @end

171cb38f4a92310d4469cc23de8bd666637e323d
muhammad fauzi masykur <muhammad.masykur@gojek.com>
2024-05-15 10:04:18 +0700

    add model for kredivo

---
+++ MidtransCoreKit/MidtransCoreKit/MidtransPaymentKredivo.h
@@ -0,0 +1,17 @@
+//
+//  MidtransPaymentKredivo.h
+//  MidtransCoreKit
+//
+//  Created by Muhammad Masykur on 14/05/24.
+//  Copyright © 2024 Midtrans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MidtransPaymentKredivo : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MidtransCoreKit/MidtransCoreKit/MidtransPaymentKredivo.m
@@ -0,0 +1,13 @@
+//
+//  MidtransPaymentKredivo.m
+//  MidtransCoreKit
+//
+//  Created by Muhammad Masykur on 14/05/24.
+//  Copyright © 2024 Midtrans. All rights reserved.
+//
+
+#import "MidtransPaymentKredivo.h"
+
+@implementation MidtransPaymentKredivo
+
+@end

7507109d4a137776cf216c5c1bc60f8219361d36
Muhammad Fauzi Masykur <muhammad.masykur@gojek.com>
2026-02-18 12:23:23 +0700

    bump version to 1.29.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "MidtransCoreKit", url: "https://github.com/veritrans/Midtrans-xcframework/releases/download/1.28.0/MidtransCoreKit.xcframework-1.28.0.zip", checksum: "7834333a072988fd87acdee448932f906321e3ccc842e707b17d46f19c72a3eb"),
-        .binaryTarget(name: "MidtransKit", url: "https://github.com/veritrans/Midtrans-xcframework/releases/download/1.28.0/MidtransKit.xcframework-1.28.0.zip", checksum: "f8cfe766582842df6ab53ef853616f0ba6db2deddf3584990a017968907df31f")
+        .binaryTarget(name: "MidtransCoreKit", url: "https://github.com/veritrans/Midtrans-xcframework/releases/download/1.28.0/MidtransCoreKit.xcframework-1.29.0.zip", checksum: "a78ae72955cbb4620400714018de9104eabbabca26f5a40eb0fcdb0a20ecdcad"),
+        .binaryTarget(name: "MidtransKit", url: "https://github.com/veritrans/Midtrans-xcframework/releases/download/1.28.0/MidtransKit.xcframework-1.29.0.zip", checksum: "f90213700cdea173b08a0605468c4fa4eeb191627ac8220238056a04bc8a86fe")
     ]
 )
